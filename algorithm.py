8e08af36c5bab267bba6fa6e8932501922da7d0c
Sergey Vernidub <svernidub@gmail.com>
2012-11-02 12:16:27 +0200

    Нуждается в проверке

--- algorithm.py
+++ algorithm.py
@@ -15,6 +15,8 @@ class algorithm:
 
 	parents = []
 
+	descendants = []
+
 # initialize the first population and fitness calculation
 	def __init__(self,size,parents_n): 
 		self.pop_size = size
@@ -34,17 +36,87 @@ class algorithm:
 
 # selection
 	def selection(self):
-		self.parents = self.population[0:self.pop_size/100*self.parents_num]
+		num = self.parents_num
+		i = 0
+		self.parents = []
+		while i<num:
+			parent = []
+			parent.append(self.population[i])
+			if i+1<num:
+				parent.append(self.population[i+1])
+			else:
+				parent.append(self.population[0])
+			self.parents.append(parent)
+			i+=2
 
 
 # crossingover
+	def crossingover(self):
+		n = 0
+		self.descendants = []
+
+		while n < len(self.parents):
+			parent1 = float_to_binary(self.parents[n][0]['value'])
+			parent2 = float_to_binary(self.parents[n][1]['value'])
+
+			start_cross = random.randint(0,31)
+			stop_cross = random.randint(32,63)
+			descendant1_chromosome = parent1[:start_cross]+parent2[start_cross:stop_cross]+parent1[stop_cross:]
+			descendant2_chromosome = parent2[:start_cross]+parent1[start_cross:stop_cross]+parent2[stop_cross:]
+
+			d1 = {}
+			d2 = {}
+
+			d1['value'] = binary_to_float(descendant1_chromosome)
+			d2['value'] = binary_to_float(descendant2_chromosome)
+
+			d1['fitness'] = 0
+			d2['fitness'] = 0
+
+			self.descendants.append(d1)
+			self.descendants.append(d2)
+			n += 1
 
 # mutation
+	def mutation(self):
+		Fit = fitness_function()
 
+		for x in xrange(0,len(self.descendants)):
+			chromosome = float_to_binary( self.descendants[x]['value'])
+			gen = random.randint(32,63)
+
+			if chromosome[gen] == '0':
+				chromosome = chromosome[:gen] + '1' + chromosome[gen+1:]
+			else:
+				chromosome = chromosome[:gen] + '1' + chromosome[gen+1:]
+
+			self.descendants[x]['value'] = binary_to_float(chromosome)
+			self.descendants[x]['fitness'] = Fit.f(self.descendants[x]['value'])
+
+# reduction
+	def reduction(self):
+		for x in xrange(0,len(self.descendants)):
+			self.population.append(self.descendants[x])
+		self.population.sort(reverse=True)
+		self.population = self.population[:self.pop_size]
+	
+
+# population maximum
+	def pop_max(self):
+		max = -999999999
+		ind = 0
+		for x in xrange(0,self.pop_size):
+			if self.population[x]['fitness'] > max:
+				max = self.population[x]['fitness']
+				ind = x
+		print 'Population maximum in x=',
+		print self.population[ind]['value'],
+		print ' f(x)=',
+		print max
 
 
 # print the current population
-	def print_population(self,arr):
+	def print_population(self):
 		for x in xrange(0,self.pop_size):
 			print '[',x,']',
 			print self.population[x]['value'],'-',
--- main.py
+++ main.py
@@ -4,16 +4,23 @@ from algorithm import algorithm
 
 
 start_population_size = int(input('Eneter the size of initial population: '))
-
-parents_n = int(input('Enter the number of parents, %: '))
+parents_n = -100
+while parents_n <= 0 or parents_n > start_population_size:
+	parents_n = int(input('Enter the number of parents in population (0<parents<size_of_initial): '))
 	
 epochn = int(input('Enter the number of the epoches: '))
 
 
 alg = algorithm(start_population_size,parents_n)
-epoch = 1
-#while epoch <= epochn:
+epoch = 0
+while epoch < epochn:
+	print 'Epoch #',epoch
+	alg.pop_max()
 	alg.selection()
+	alg.crossingover()
+	alg.mutation()
+	alg.reduction()
+	epoch += 1
 
 
 
